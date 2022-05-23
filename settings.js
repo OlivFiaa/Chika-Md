1ff42f08767dc31ca0774f0b9d55c1269a87f911
Firrlyx <106093347+Firrlyx@users.noreply.github.com>
2022-05-23 21:32:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 /*
-› Create By @RIY
+› Create By @firahhe
 › Base Ori @DikaArdnt
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
@@ -22,27 +22,26 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6288225916922','6288225916922','6288225916922'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6288225916922'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6288225916922' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Fira' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Firlx' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Fira' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
+global.youtube = 'https://youtube.com/channel/UCwVntyyEMhDHXIB_hj8zizA'
 global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'riychdwaynee@gmail.com'
-global.region = 'indonesia'
+global.mygc = 'https://chat.whatsapp.com/Dkv8M9nOGsP2b7xPgzZApi'
+global.email = 'oxsfrea@gmail.com'
+global.region = 'japan'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Firlx' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Fira' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
